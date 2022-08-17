99a9c96ebd313f6aff491f8a9b7b1741fa2e305a
marta0220 <109087240+marta0220@users.noreply.github.com>
2022-08-18 00:24:09 +0300

    Final touches

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Forecast.css";
 import axios from "axios";
 import ForecastDay from "./ForecastDay";
 
-export default function Parameters(props) {
+export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.data.coords]);
+
   function handleResponse(response) {
     setLoaded(true);
     setForecast(response.data.daily);
@@ -20,6 +25,8 @@ export default function Parameters(props) {
                 <ForecastDay data={dailyForecast} />
               </div>
             );
+          } else {
+            return null;
           }
         })}
       </div>
@@ -28,7 +35,6 @@ export default function Parameters(props) {
     const apiKey = "d873f8799b310a5282754959e9912176";
     let latitude = props.data.coords.lat;
     let longitude = props.data.coords.lon;
-
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
